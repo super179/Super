aa0a306bf7bd9526a628ada6b9dd2784d83583f7
super <179395633@qq.com>
2023-09-22 15:31:53 +0800

    提交测试

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+
+use hw\common\base\Application;
+
+/**
+ * IDE辅助提示
+ * author: WCY
+ * date: 2021/04/21 11:14 PM
+ * version: 1.0
+ */
+class Yii
+{
+    /**
+     * @var Application
+     */
+    public static $app;
+}
